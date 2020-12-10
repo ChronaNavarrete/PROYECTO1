d1b5387a110338ae2bc9708f93ada19042ed39c7
Andreu <2001.lechuga@gmail.com>
2020-12-09 22:00:19 -0300

    actualizando

--- respaldo/src/cabildos/methods.py
+++ respaldo/src/cabildos/methods.py
@@ -1,4 +1,26 @@
+from quickchart import QuickChart
 
-
-def crearpost(conclusion, aprobacion):
+def crearpost(id_cabildo, realizado): # realizado es un booleano 
+    if not realizado:
+        pass
     pass
+
+def creargrafico():
+    qc = QuickChart()
+    qc.width = 500
+    qc.height = 300
+    qc.device_pixel_ratio = 2.0
+    qc.config = { 
+        "type": "bar",
+        "data": {
+            "labels": ["Hello world", "Test"],
+            "datasets": [{
+                "label": "Foo",
+                "data": [1, 2]
+            }]
+        }
+    }
+    return qc.get_url()
+
+img = creargrafico()
+print(img)
