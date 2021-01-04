b7ef33b23e6745924bd46e3707cd3a6dab6ff172
Andreu <2001.lechuga@gmail.com>
2021-01-04 01:47:37 -0300

    retoque casi todo lo ultimo relacionado a articulos

--- respaldo/src/posts/forms.py
+++ respaldo/src/posts/forms.py
@@ -20,7 +20,10 @@ class PostForm(forms.ModelForm):
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields =('content', )
+        fields =['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'class':'form-control'}),
+        }
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
