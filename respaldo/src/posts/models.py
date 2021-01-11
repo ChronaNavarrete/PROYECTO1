077393493a1f4f14ada420d7eedee40dabf9c3ea
Andreu <2001.lechuga@gmail.com>
2021-01-11 00:19:21 -0300

    lo ultimo de la reunion

--- respaldo/src/posts/models.py
+++ respaldo/src/posts/models.py
@@ -19,7 +19,7 @@ class User(AbstractUser):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default= 'default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default= 'img-perfil-ejemplo.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
